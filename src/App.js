d8f84abad281872279289fae7ef354f5bb3891e2
[Sital Pun] <[shitalmagar21@gmail.com]>
2023-08-20 13:06:23 -0400

    Build fixed

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import "./Weather.css";
 function App() {
   return (
     <div className="App">
-      //{" "}
       <div className="container">
-        //{" "}
         <div className="app-container-wrapper">
           <Weather />
           <small className="github-link">
