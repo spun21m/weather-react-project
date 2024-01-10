88b30206f449cd67fef822b8834024ce9cb7cce6
[Sital Pun] <[shitalmagar21@gmail.com]>
2024-01-09 19:29:29 -0500

    Display forecast for 5 days

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,9 +16,15 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col">
-            <WeatherForecastDay data={forecastData[0]}/>
-          </div>
+          {forecastData.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
